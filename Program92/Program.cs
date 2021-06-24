4c88949d22edd4e4cc25cd86e6b6bfffa204202c
alexandrumalinescu <67902199+alexandrumalinescu@users.noreply.github.com>
2021-06-24 18:40:21 +0300

    Add project files.

---
+++ Program92/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Program92
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputData= Console.ReadLine();
+            char letter = Console.ReadLine()[0];
+            bool check = false;
+
+            for (int i=0; i<inputData.Length; i++)
+           
+                if (inputData[i] == letter)
+                    check= true;
+            Console.WriteLine(check);
+        }
+    }
+}
